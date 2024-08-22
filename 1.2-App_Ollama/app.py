ad57360dcdca2dc63ddf9687672d55798e031af1
srirampamerla <53964156+srirampamerla@users.noreply.github.com>
2024-08-22 18:36:08 +0530

    Simple GENAI APP using Ollama_Deploy in Streamlit

---
+++ 1.2-App_Ollama/app.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+# Langsmith tracking in the langsmith website
+os.environ['LANGCHAIN_API_KEY']=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+
+
+# prompt template
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to the question asked"),
+        ("user","Question:{question}")  
+
+    ]
+)
+
+## stream lit framework
+
+st.title("Langchain Demom with gemma2:2b model")
+input_text=st.text_input("What question you have in mind?")
+
+##Ollama gemma2:2b model
+
+llm=Ollama(model="gemma2:2b")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
